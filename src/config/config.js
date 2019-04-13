4de29297417007c4f58f3be759f50ea53998229c
KaramMahdi <karammah28@gmail.com>
2019-04-13 19:20:07 +0300

    pagination

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
-export const list_of_categories = 'http://one-drupal-demo2.technikh.com/onedrupal/api/v1/vocabulary/categories';
-export  const all_posts="http://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all";
-export  const all_css_posts="http://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/17";
-export  const all_js_posts="http://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/19";
+export const list_of_categories = 'https://one-drupal-demo2.technikh.com/onedrupal/api/v1/vocabulary/categories';
+export  const all_posts="https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all";
+export  const all_css_posts="https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/17";
+export  const all_js_posts="https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/19";
 export const list_of_category_type=(id)=>{
 
     return "https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/"+id+"";
