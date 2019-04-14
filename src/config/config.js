df305df5400dff2bb166a62841987185332db59b
Nikhil Dubbaka <info@technikh.com>
2019-04-14 08:28:58 -0400

    site change from demo2 to demo

--- src/config/config.js
+++ src/config/config.js
@@ -1,24 +1,24 @@
 export const list_of_categories =(reactjs_blog_categorySetting)=>
-{return 'https://one-drupal-demo2.technikh.com/onedrupal/api/v1/vocabulary/'+reactjs_blog_categorySetting+''}
-export  const all_css_posts="https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/17";
-export  const all_js_posts="https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/19";
-export  const setting_api="https://one-drupal-demo2.technikh.com/onedrupal/api/v1/settings";
+{return 'https://one-drupal-demo.technikh.com/onedrupal/api/v1/vocabulary/'+reactjs_blog_categorySetting+''}
+export  const all_css_posts="https://one-drupal-demo.technikh.com/onedrupal/api/v1/content/all/17";
+export  const all_js_posts="https://one-drupal-demo.technikh.com/onedrupal/api/v1/content/all/19";
+export  const setting_api="https://one-drupal-demo.technikh.com/onedrupal/api/v1/settings";
 
 export const list_of_category_type=(id,pagenum)=>{
 
-    return "https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all/"+id+"?page="+pagenum+"";
+    return "https://one-drupal-demo.technikh.com/onedrupal/api/v1/content/all/"+id+"?page="+pagenum+"";
 }
 
 export const category_details=(id)=>{
 
-    return "https://one-drupal-demo2.technikh.com/node/"+id+"?_format=json";
+    return "https://one-drupal-demo.technikh.com/node/"+id+"?_format=json";
 }
 export const category_type=(id)=>{
 
-    return "https://one-drupal-demo2.technikh.com/taxonomy/term/"+id+"?_format=json";
+    return "https://one-drupal-demo.technikh.com/taxonomy/term/"+id+"?_format=json";
 }
 export const all_posts=(pagenum)=>{
 
-    return "https://one-drupal-demo2.technikh.com/onedrupal/api/v1/content/all?page="+pagenum+"";
+    return "https://one-drupal-demo.technikh.com/onedrupal/api/v1/content/all?page="+pagenum+"";
 }
 
