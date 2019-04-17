a7c24d0a2990711875b6eb4cd13dc0a7e2cc47fb
TechNikh <nd49@njit.edu>
2019-04-17 17:40:21 -0400

    removed code duplicates in index.js & category.js

--- src/components/category.js
+++ src/components/category.js
@@ -11,6 +11,8 @@ import ScrollAnimation from 'react-animate-on-scroll';
 
 import "animate.css/animate.min.css";
 
+import { processContentListApi } from "./drupal-util";
+
 
 class category extends Component {
     constructor(props)
@@ -123,84 +125,8 @@ fetchDataAPI2=(page_num)=>{
           fetch(setting_api)
             .then(blob3 => blob3.json())
             .then(data3 => {
-     var article_body="",article_image="",article_category="",article_type="";
-                           
-
-                             for(var x=0;x<data3.types.length;x++)
-                                 {
-     article_body=data3.types[x].fields.body;
-    article_image=data3.types[x].fields.image;
-     article_type=data3.types[x].node_type;   
-                                     if(data3.types[x].fields.taxonomies.length>0)
-                                         {
-                                                 article_category=data3.types[x].fields.taxonomies[0].field;
-
-                                         }
-        for(var i=0;i<mainmenu.length;i++)
-            {
-  
-            //image, typeId
-                if(mainmenu[i].type[0].target_id ===article_type){
-                         var typeId="",typename="",image="";
-      var day="",month="",year="",fulldate="";
-    var datefull = new Date(mainmenu[i].changed[0].value.toString());
-              
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-        day = datefull.getDate();
-            month =monthNames[datefull.getMonth()];
-             year = datefull.getFullYear();
-            fulldate=month+" "+day+" "+year;
-                       var bodeImage="";          
-                        if(article_image.length>0){
-                            if(mainmenu[i][article_image] !=undefined){
-                                if(mainmenu[i][article_image].length>0){
-                 bodeImage=mainmenu[i][article_image][0].url
-                                }
-
-                               }   
-                        }
-                    if(mainmenu[i][article_category] !=undefined)
-                       {
-                           if(mainmenu[i][article_category].length>0){
-                       typeId=mainmenu[i][article_category][0].target_id;
-                           }
-                       }
-                    
-                    
-                                  let blogs = {
-                                nid: mainmenu[i].nid[0].value,
-                          typenameid:i,
-                                typeId:typeId,
-                                title: mainmenu[i].title[0].value,
-                                image: bodeImage,
-                                date:fulldate, 
-                                type:""
-
-                            };
-                    console.log(typeId);
-                          mainmenustate.push(blogs);
-
-                          
-
-                            
-                    
-                    
-                   }
-                
-                
-                
-                
-        }
-                                     
-                     this.setState({blogs:mainmenustate});                
-                                     
-                                     
-                                     
-                                 }
-                   
-               
+				var processedData = processContentListApi(data3, mainmenu);
+				this.setState(processedData);
            });
          
      });
