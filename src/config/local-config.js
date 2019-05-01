1424246f113cf1fbd0092280806a097ba8d1ca2f
TechNikh <nd49@njit.edu>
2019-05-01 08:11:21 -0400

    domain change

--- src/config/local-config.js
+++ src/config/local-config.js
@@ -1,5 +1,5 @@
 
-export  const l_domain="https://nikhil.dubbaka.com"; // no trailing slash
+export  const l_domain="https://content.dubbaka.com"; // no trailing slash
 export  const l_sitename="Nikhil Dubbaka";
 export  const l_twitter_handle="TechNikh";
 export  const l_facebook_handle="";
@@ -10,8 +10,8 @@ export  const l_sidebar_block1_tid=26;
 export  const l_sidebar_block2_title="Drupal Quotes";
 export  const l_sidebar_block2_tid=31;
 
-export  const l_footer_block1_title="Drupal 8";
-export  const l_footer_block1_tid=22;
+export  const l_footer_block1_title="Drupal Quotes";
+export  const l_footer_block1_tid=31;
 
 export  const l_footer_block2_title="Drupal Memes";
 export  const l_footer_block2_tid=26;
